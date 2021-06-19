37c10ad19f6cbcabe0e6c7f04bfdf7b19c598e1b
mak <faren5128@gmail.com>
2021-06-19 15:24:30 +0300

    new_main1

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ if __name__ == '__main__':
     arry_comand = ['save_file', 'new_fail', 'open_file', 'delit_file']
     print("Команды программы: ", arry_comand)
     while True:
-        str1 = input('Введите команду: ')
+        str1 = input('Введите команду: ').strip()
         if str1 == 'exit':
             print("Exit")
+            break
         elif str1 == arry_comand[1]:
             print("Выполнилась команда: ", new_file())
