0688bb1546897153905fe1cfa23aea1e26bb559a
mak <faren5128@gmail.com>
2021-06-19 21:23:30 +0300

    new_funzia3

--- funcia_scv.py
+++ funcia_scv.py
@@ -1,14 +1,14 @@
 class csv(object):
 
-    def initializ(self, file, neew_colum):
+    def initializ(self, file, new_colon):
         pass
 
 
 def new_file():
-    file_neme = input("Enter file name: ")
+    file_neme = input("Enter file name: ").strip()
     columns = input("Enter name columns ,: ")
     column = columns.split(',')
     new_colum = [col.strip() for col in column]
     file = csv()
-    file.initializ(file_neme, new_colum)
-    print("Create file with colums: ", file)
+    new_fil = file.initializ(file_neme, new_colum)
+    print("Create file with colums: ", new_fil)
