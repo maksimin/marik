610202a02c178d80cd3cda0e18dcd7853255e9ca
mak <faren5128@gmail.com>
2021-06-19 15:24:58 +0300

    new_funzia1

--- funcia_scv.py
+++ funcia_scv.py
@@ -1,6 +1,9 @@
+class csv:
+
+    
+
 def new_file():
     file_neme = input("Enter file name: ")
     columns = input("Enter name columns ,: ")
     column = columns.split(',')
     new_colum = [col.strip() for col in column]
-    return file_neme, new_colum
