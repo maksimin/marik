6538f7905b6fee0943f490b505e510f01111b926
mak <faren5128@gmail.com>
2021-06-20 12:07:48 +0300

    new comit csv

---
+++ Csv.py
@@ -0,0 +1,32 @@
+class Csv:
+
+    def __init__(self, fail_name, new_colun, lines):
+        self.fail_name = fail_name
+        self.new_colun = new_colun
+        self.lines = []
+
+    def open(self, file):
+        self.open(file)
+
+    def seve(self):
+        pass
+
+    def add(self, lin):
+        self.new_lin = input("Enter lines ,: ")
+
+        self.lines
+
+    def delete(self):
+        pass
+
+    def delete_column(self):
+        pass
+
+    def add_column(self):
+        pass
+
+    def print(self):
+        pass
+
+    def __str__(self):
+        return f"{self.fail_name} {self.new_colun}"
